b58edadb8044011fbad62a128e4ab57afc35d3f7
Robert Aimes <robaimes27@gmail.com>
2023-06-12 15:02:29 -0600

    Began work on login controller

--- src/sample/controller/loginFormController.java
+++ src/sample/controller/loginFormController.java
@@ -1,4 +1,25 @@
 package sample.controller;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class loginFormController {
+
+    @FXML private TextField username;
+    @FXML private TextField password;
+    @FXML private Button loginButton;
+
+
+    /*
+    This method handles the functionality when the log in button is clicked. Queries the db for correct username and password
+     */
+
+    public void loginButtonClicked(ActionEvent event) throws IOException{
+        String enteredUsername = username.getText();
+        String enteredPassword = password.getText();
+
+    }
 }
