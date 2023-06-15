2844c2057f3412aefd1afd0db0acef450b0b9c55
Robert Aimes <robaimes27@gmail.com>
2023-06-15 16:22:51 -0600

    Built main screen view and started controller

--- src/sample/controller/mainScreenController.java
+++ src/sample/controller/mainScreenController.java
@@ -1,4 +1,80 @@
 package sample.controller;
 
-public class mainScreenController {
+import com.sun.javafx.stage.EmbeddedWindow;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import sample.DAO.JDBC;
+import sample.main.Main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javafx.fxml.Initializable;
+
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.time.ZoneId;
+import java.util.ResourceBundle;
+import java.util.Locale;
+import java.util.TimeZone;
+
+
+
+
+public class mainScreenController implements Initializable{
+
+    @FXML private RadioButton appointmentByWeekRadio;
+    @FXML private RadioButton appointmentByMonthRadio;
+    @FXML private RadioButton appointmentAllRadio;
+    @FXML private TableView appointmentTable;
+    @FXML private TableView customerTable;
+    @FXML private TableColumn appointmentIdColumn;
+    @FXML private TableColumn appointmentTitleColumn;
+    @FXML private TableColumn appointmentDescriptionColumn;
+    @FXML private TableColumn appointmentLocationColumn;
+    @FXML private TableColumn appointmentStartColumn;
+    @FXML private TableColumn appointmentEndColumn;
+    @FXML private TableColumn appointmentCustomerIdColumn;
+    @FXML private TableColumn appointmentContactColumn;
+    @FXML private TableColumn appointmentTypeColumn;
+    @FXML private TableColumn appointmentUserIdColumn;
+    @FXML private TableColumn customerIdColumn;
+    @FXML private TableColumn customerNameColumn;
+    @FXML private TableColumn customerAddressColumn;
+    @FXML private TableColumn customerPostalColumn;
+    @FXML private TableColumn customerPhoneColumn;
+    @FXML private TableColumn customerStateColumn;
+    @FXML private TableColumn customerCreatedDateColumn;
+    @FXML private TableColumn customerCreatedByColumn;
+    @FXML private TableColumn customerLastUpdateColumn;
+    @FXML private TableColumn customerLastUpdatedByColumn;
+    @FXML private Button addApptButton;
+    @FXML private Button updateApptButton;
+    @FXML private Button deleteApptButton;
+    @FXML private Button adjustApptTimeButton;
+    @FXML private Button addCustomerButton;
+    @FXML private Button modifyCustomerButton;
+    @FXML private Button deleteCustomerButton;
+    @FXML private Button reportButton;
+    @FXML private Button logoutButton;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
