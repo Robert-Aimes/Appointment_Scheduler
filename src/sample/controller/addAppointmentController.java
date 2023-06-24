301fe496813254182d3bbd24de0962b2c7772930
Robert Aimes <robaimes27@gmail.com>
2023-06-24 13:22:05 -0600

    populating data in combo boxes in addAppointmentController

--- src/sample/controller/addAppointmentController.java
+++ src/sample/controller/addAppointmentController.java
@@ -1,5 +1,7 @@
 package sample.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,8 +10,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import sample.DAO.ContactsDb;
+import sample.DAO.CustomerDb;
+import sample.DAO.UsersDb;
+import sample.model.Contacts;
+import sample.model.Customer;
+import sample.model.Users;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class addAppointmentController {
     @FXML
@@ -31,11 +40,11 @@ public class addAppointmentController {
     @FXML
     private ComboBox addApptEndTimeChoice;
     @FXML
-    private ComboBox addApptCustomerIdChoice;
+    private ComboBox<Integer> addApptCustomerIdChoice;
     @FXML
-    private ComboBox addApptUserIdChoice;
+    private ComboBox<Integer> addApptUserIdChoice;
     @FXML
-    private ComboBox addApptContactChoice;
+    private ComboBox<String> addApptContactChoice;
     @FXML
     private Button addApptSaveButton;
     @FXML
@@ -52,5 +61,34 @@ public class addAppointmentController {
             window.setScene(mainScreenScene);
             window.show();
         }
+    }
+
+    @FXML
+    public void initialize() throws SQLException {
+        ObservableList<Contacts> contactsList = ContactsDb.getAllContacts();
+        ObservableList<String> contactNamesList = FXCollections.observableArrayList();
+        for(Contacts contact : contactsList){
+            String contactName = contact.getContactName();
+            contactNamesList.add(contactName);
+        }
+
+        ObservableList<Users> userList = UsersDb.getAllUsers();
+        ObservableList<Integer> userIDList = FXCollections.observableArrayList();
+        for(Users user : userList){
+            int userID = user.getUserID();
+            userIDList.add(userID);
+        }
+
+        ObservableList<Customer> customerList = CustomerDb.getAllCustomers();
+        ObservableList<Integer> customerIDList = FXCollections.observableArrayList();
+        for(Customer customer : customerList){
+            int customerID = customer.getCustId();
+            customerIDList.add(customerID);
+        }
+
+        addApptContactChoice.setItems(contactNamesList);
+        addApptUserIdChoice.setItems(userIDList);
+        addApptCustomerIdChoice.setItems(customerIDList);
+
     }
 }
