5428fe42257e968e0976e6943ff0ee5ff4738665
Robert Aimes <robaimes27@gmail.com>
2023-07-01 13:59:21 -0600

    v1 Complete

--- src/sample/DAO/ContactsDb.java
+++ src/sample/DAO/ContactsDb.java
@@ -2,7 +2,6 @@ package sample.DAO;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import jdk.internal.jimage.ImageStrings;
 import sample.model.Appointment;
 import sample.model.Contacts;
 
